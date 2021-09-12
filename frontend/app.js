ea6c7ba4f6ef866b233dfbf180a17404d2d39c3f
zunair khan <zunairkhan0@gmail.com>
2021-09-12 20:54:07 +0200

    added basic markup

---
+++ frontend/app.js
@@ -0,0 +1,12 @@
+const upload_btn = document.getElementById("upload-btn")
+
+
+
+upload_btn.addEventListener("click",btn_handler)
+
+
+
+
+function btn_handler(){
+    console.log("btn clicked");
+}
